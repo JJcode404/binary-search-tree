b4bb92585baf37c779eeef9d30b49d6496da1c41
thatboikham <khamjapher1@gmail.com>
2024-08-20 22:16:22 +0300

    feat: creat a bts function that returns node

--- bts.js
+++ bts.js
@@ -5,3 +5,15 @@ class Node {
         this.right = null
     }
 };
+
+function sortedArraytoBts(array,start,end){
+    if(start > end){
+        return null;
+    }
+    const mid = parseInt((start + end)/2)
+    const node = new Node(array[mid])
+    node.left = sortedArraytoBts(array,start,mid-1)
+    node.right = sortedArraytoBts(array,mid+1,end)
+    
+    return node;
+};
